fa512bc025bfdc6953c72bdb96bff0020d7afa60
mrqken <kenma95@gmail.com>
2015-11-24 15:51:46 -0500

    add test case defrag

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import copy
 ##############
 class Proccess:
    def __init__(self,proc):
-      self.proc_num = int (proc[0])
+      self.proc_num =  (proc[0])
       self.arrival_time = int(proc[1])
       self.burst_time = int (proc[2])
       self.burst_time_remain= int (proc[2])
@@ -36,13 +36,170 @@ def print_queue(queue,proc_list):
     printout+="]"
     return printout
 
+#add_memory is called the first time a process enter the ready queue
+#p: Process object that enter the queue
+#algor: Placement Algorithm
+#           - 0 for first fit, 1 for next-fit, 2 for best fit
+# previous: previous add_memory end location, only used in algor=1
+#return: return the 'previous' add_memory end location prepare for the next process added
+#           -1 for fail to add, need to defrag
+def add_memory(p, memory_map,algor,previous):
+    global m_map
+    start_loc = -1
+    end_loc =-1
+    if algor == 0:    #first fit
+
+        i = 0
+        while i<256:
+            if m_map[i] == '.' and start_loc == -1:   #locate start
+                start_loc = i
+            if start_loc != -1 and m_map[i] != '.':  #locate end
+                end_loc = i 
+                if end_loc - start_loc >= p.memory:     #long enough to hold
+                    add_memory_exec(start_loc, p)
+                    return start_loc + p.memory 
+                else:   #not long enough
+                    start_loc = -1
+                    end_loc = -1
+
+            i+=1
+        if start_loc != -1 and end_loc == -1:
+            end_loc =256
+        if end_loc - start_loc >= p.memory:     #long enough to hold
+            add_memory_exec(start_loc,p)
+            return start_loc + p.memory 
+
+    if algor == 1:  #next fit
+        i = previous
+        while i<256:
+            if m_map[i] == '.' and start_loc == -1:   #locate start
+                start_loc = i
+            if start_loc != -1 and m_map[i] != '.':  #locate end
+                end_loc = i
+                if end_loc - start_loc >= p.memory:     #long enough to hold
+                    add_memory_exec(start_loc, p)
+                    return start_loc + p.memory
+                else:   #not long enough
+                    start_loc = -1
+                    end_loc = -1
+            i+=1
+        if start_loc != -1 and end_loc == -1:
+            end_loc =256
+        if end_loc - start_loc >= p.memory:     #long enough to hold
+            add_memory_exec(start_loc,p)
+            return start_loc + p.memory
+        #start from head
+        i =0
+        while i<previous:
+            if m_map[i] == '.' and start_loc == -1:   #locate start
+                start_loc = i
+            if start_loc != -1 and m_map[i] != '.':  #locate end
+                end_loc = i
+                if end_loc - start_loc >= p.memory:     #long enough to hold
+                    add_memory_exec(start_loc,p)
+                    return start_loc + p.memory
+                else:   #not long enough
+                    start_loc = -1
+                    end_loc = -1
+
+    if algor ==2:  #best fit
+        start_l =[]
+        end_l = []
+        i = 0
+        while i<256:
+            if m_map[i] == '.' and start_loc == -1:   #locate start
+                start_loc = i
+            if start_loc != -1 and m_map[i] != '.':  #locate end
+                end_loc = i 
+                if end_loc - start_loc >= p.memory:     #long enough to hold
+                    start_l.append(start_loc)
+                    end_l.append(end_loc)
+                else:   #not long enough
+                    start_loc = -1
+                    end_loc = -1
+
+            i+=1
+        if start_loc != -1 and end_loc == -1:
+            end_loc =256
+        if end_loc - start_loc >= p.memory:     #long enough to hold
+            start_l.append(start_loc)
+            end_l.append(end_loc)
+
+        if len(start_l)>0:
+            suitable = 0
+            for i in range(0,len(start_l)):
+                if end_l[i]-start_l[i] < end_l[suitable]-start_l[suitable]:
+                    suitable = i
+            add_memory_exec(start_l[suitable],p)
+            return start_l[suitable]+p.memory
+
+    #not yet return, meaning no enough space
+    return -1  #return -1 for defrag signal
+
+#sub function of add_memory
+def add_memory_exec(start_loc, p):
+    global m_map
+    m_map = m_map[:start_loc]+p.proc_num * p.memory +m_map[start_loc+p.memory:]
+
+
+#call when a process is terminated, clean the memeory
+def remove_memory(p):
+    global m_map
+    start_loc = -1
+    end_loc = -1
+    
+
+    for i in range(0,256):
+        if m_map[i] == str(p.proc_num) and start_loc == -1:
+            start_loc = i
+        if start_loc != -1 and m_map[i] != p.proc_num:
+            end_loc = i
+            break
+    print start_loc,end_loc
+    m_map = m_map[0:start_loc]+'.' * p.memory +m_map[end_loc:256]
+
+
+
+#defragmentation function
+#return the cost of defrag
+def defraging():
+    global m_map
+    back_up=[]
+    for i in range(0,256):
+        if m_map[i]!= '.':
+            back_up.append(m_map[i])
+    while len(back_up)!=256:
+        back_up.append('.')
+    count =0
+    for i in range(0,256):
+        if back_up[i]!= '.' and back_up[i]!=m_map[i]:
+            count += 1
+    m_map = back_up
+    return count
+
+
+def print_mem(t):
+    global m_map
+    print "time "+str(t)+"ms: Simulated Memory:"
+    print "="*32,
+    for i in range(0,256):
+        if (i%32 == 0):
+            print "\n",
+        print m_map[i],
+    print "\n"+"="*32 +"\n"
+
+        
+
+
 
 ##############################
 #SRT
 ##############################
 def get_next_interest_srt(burst_time_remain,switching,current_time):
-    global io_list,time,proc_list_SRT
+    global io_list,time,proc_list_SRT,defrag,defrag_finished_time
     t_time = 999
+    if defrag:
+        t_time = defrag_finished_time
     for item in proc_list_SRT:
         if item.arrival_time > time:
             t_time = min(item.arrival_time - time,t_time)
@@ -63,22 +220,37 @@ def get_next_interest_srt(burst_time_remain,switching,current_time):
 def time_pass_srt(t_time):
     global time, burst_time_remain, proc_list_SRT,current_burst,\
     q_burst,io_list,switching,t_cs,\
-    stat_burst_time,stat_cs,stat_turnaround,stat_waittime
-    
-    add_proc=0
+    stat_burst_time,stat_cs,stat_turnaround,stat_waittime,previous,defrag,\
+    defrag_finished_time,algor
+        
     skip_io_flag=0
     time+=t_time
+    if (time == defrag_finished_time and defrag == 1):
+        defrag = 0
     #turnaround stat
     if current_burst != -1:
         stat_turnaround += t_time
     stat_turnaround += len(q_burst) * t_time
     #system check
-    for item in proc_list_SRT:
-        if item.arrival_time == time:
-            q_burst.append(item)
+    for i in range(0,len(proc_list_SRT)):
+        if proc_list_SRT[i].arrival_time == time and not defrag:
+            q_burst.append(i)
+            print "time "+ str(time) + "ms: P"+str(proc_list_SRT[i].proc_num) +"  added to system"+print_queue(q_burst,proc_list_SRT)
+            previous = add_memory(proc_list_SRT[i],m_map,algor,previous)
+            #print "previous is ",previous
+            if previous == -1:
+                #defrag
+                defrag = 1;
+                print "DEFRAGING\n"
+                defrag_finished_time = defraging() + time
+            print_mem(time)
+        if proc_list_SRT[i].arrival_time == time and defrag:
+            proc_list_SRT[i].arrival_time = defrag_finished_time
+
+    #raw_input()
     q_burst.sort(key = lambda a:proc_list_SRT[a].burst_time_remain)
 
-    if (switching!=0):  #in context switch
+    if (switching!=0 and not defrag):  #in context switch
         switching-=t_time
         if (switching == 0):    #ready for switch
             if (q_burst):
@@ -90,7 +262,7 @@ def time_pass_srt(t_time):
                 print "time "+ str(time) + "ms: P"+str(proc_list_SRT[current_burst].proc_num) +" started using the CPU "+print_queue(q_burst,proc_list_SRT)
                 add_proc =1
 
-    if (burst_time_remain != 0 and add_proc==0):
+    if (burst_time_remain != 0 and not defrag):
         stat_waittime += len(q_burst)*t_time
         burst_time_remain =int( burst_time_remain) - t_time
         proc_list_SRT[current_burst].burst_time_remain -=t_time
@@ -114,6 +286,9 @@ def time_pass_srt(t_time):
             #terminate if neither have io time nor burst trail
             else:
                 print "time "+ str(time) + "ms: P"+str(proc_list_SRT[current_burst].proc_num) + " terminated "+print_queue(q_burst,proc_list_SRT)
+                remove_memory(proc_list_SRT[current_burst])
+                print "time "+ str(time) + "ms: P"+str(proc_list_SRT[current_burst].proc_num) + " memory cleared "+print_queue(q_burst,proc_list_SRT)
+                print_mem(time)
             if q_burst:
                 switching = t_cs #reques a new context switch
             if q_burst and q_burst[0]==current_burst:
@@ -131,7 +306,7 @@ def time_pass_srt(t_time):
             if proc_list_SRT[index].num_burst == 0:
                 #process terminate
                 print "time "+ str(time) + "ms: P"+str(proc_list_SRT[index].proc_num) + " terminated "+print_queue(q_burst,proc_list_SRT)
-            
+                remove_memory(proc_list_SRT[index])
             else:   #process add back to q_burst
                 #refill burst time
                 proc_list_SRT[index].burst_time_remain = proc_list_SRT[index].burst_time
@@ -141,13 +316,13 @@ def time_pass_srt(t_time):
                     print "time "+ str(time) + "ms: P"+str(proc_list_SRT[index].proc_num)+" completed I/O "+print_queue(q_burst,proc_list_SRT)
                     q_burst.append(index)
                     q_burst.append(current_burst)
-                    q_burst.sort(key = lambda a: (proc_list_SRT[a].prior, proc_list_SRT[a].proc_num) )
+                    q_burst.sort(key = lambda a: (proc_list_SRT[a].proc_num) )
                     print "time "+ str(time) + "ms: P"+str(proc_list_SRT[current_burst].proc_num) + " preempted by P" + str(proc_list_SRT[index].proc_num) + " " + print_queue(q_burst[1:],proc_list_SRT)
                     current_burst = -1
                     burst_time_remain = 0
                 else:       #not preempted
                     q_burst.append(index)
-                    q_burst.sort(key = lambda a: (proc_list_SRT[a].prior, proc_list_SRT[a].proc_num) )
+                    q_burst.sort(key = lambda a: (proc_list_SRT[a].proc_num) )
                     print "time "+ str(time) + "ms: P"+str(proc_list_SRT[index].proc_num)+" completed I/O "+print_queue(q_burst,proc_list_SRT)                    
                 switching = t_cs
             io_list.pop(i)
@@ -187,7 +362,7 @@ if __name__ == "__main__":
     stat_cs = 0
     stat_turnaround = 0
     stat_waittime = 0
-
+    defrag = 0
     time = 0    #reset time counter and queue
     t_cs =13
     switching = t_cs
@@ -195,21 +370,26 @@ if __name__ == "__main__":
     io_list=[]
     burst_time_remain=0
     current_burst=-1
-
+    defrag_finished_time =0
+    previous = 0
+    algor =0
     #memory list 
     m_map = '.'*256;
 
     #initialize burst queue
     #start sim
     print "time "+str(time)+"ms: Simulator started for SRT " +print_queue(q_burst,proc_list_SRT)
-    for item in range(0, len(proc_list_SRT)):
-        if item.arrival_time == 0:
-            q_burst.append(item)
-    q_burst.sort(key = lambda a:proc_list_SRT[a].burst_time_remain)
-
-    while q_burst or io_list or current_burst != -1:
-        t=get_next_interest_srt(burst_time_remain,switching);
+    # for item in range(0, len(proc_list_SRT)):
+    #     if proc_list_SRT[item].arrival_time == 0:
+    #         q_burst.append(item)
+    # q_burst.sort(key = lambda a:proc_list_SRT[a].burst_time_remain)
+    start =1
+    while q_burst or io_list or current_burst != -1 or start ==1:
+        t=get_next_interest_srt(burst_time_remain,switching,time);
+        if (start == 1):
+            t =0
         time_pass_srt(t);
+        start =0
     #end sim
     print "time "+str(time)+"ms: Simulator for SRT ended " + print_queue(q_burst,proc_list_SRT) +"\n\n"
     #write to text
@@ -232,133 +412,3 @@ if __name__ == "__main__":
     f_out.close()
 
 
-#add_memory is called the first time a process enter the ready queue
-#p: Process object that enter the queue
-#algor: Placement Algorithm
-#           - 0 for first fit, 1 for next-fit, 2 for best fit
-# previous: previous add_memory end location, only used in algor=1
-#return: return the 'previous' add_memory end location prepare for the next process added
-#           -1 for fail to add, need to defrag
-def add_memory(p, memory_map,algor,previous = 0):
-    global m_map
-    start_loc = -1
-    end_loc =-1
-    if algor == 0:    #first fit
-        i = 0
-        while i<256:
-            if i == '.' and start_loc == -1:   #locate start
-                start_loc = i
-            if start_loc != -1 and i != '.':  #locate end
-                end_loc = i 
-                if end_loc - start_loc >= p.memory:     #long enough to hold
-                    add_memory_exec(start_loc, p)
-                    return start_loc + p.memory 
-                else:   #not long enough
-                    start_loc = -1
-                    end_loc = -1
-
-            i+=1
-        if start_loc != -1 and end_loc == -1:
-            end_loc =256
-        if end_loc - start_loc >= p.memory:     #long enough to hold
-            add_memory_exec(start_loc,p)
-            return start_loc + p.memory 
-
-    if algor == 1:  #next fit
-        i = previous
-        while i<256:
-            if i == '.' and start_loc == -1:   #locate start
-                start_loc = i
-            if start_loc != -1 and i != '.':  #locate end
-                end_loc = i
-                if end_loc - start_loc >= p.memory:     #long enough to hold
-                    add_memory_exec(start_loc, p)
-                    return start_loc + p.memory
-                else:   #not long enough
-                    start_loc = -1
-                    end_loc = -1
-        if start_loc != -1 and end_loc == -1:
-            end_loc =256
-        if end_loc - start_loc >= p.memory:     #long enough to hold
-            add_memory_exec(start_loc,p)
-            return start_loc + p.memory
-        #start from head
-        i =0
-        while i<previous:
-            if i == '.' and start_loc == -1:   #locate start
-                start_loc = i
-            if start_loc != -1 and i != '.':  #locate end
-                end_loc = i
-                if end_loc - start_loc >= p.memory:     #long enough to hold
-                    add_memory_exec(start_loc,p)
-                    return start_loc + p.memory
-                else:   #not long enough
-                    start_loc = -1
-                    end_loc = -1
-
-    if algor ==2:  #best fit
-        start_l =[]
-        end_l = []
-        i = 0
-        while i<256:
-            if i == '.' and start_loc == -1:   #locate start
-                start_loc = i
-            if start_loc != -1 and i != '.':  #locate end
-                end_loc = i 
-                if end_loc - start_loc >= p.memory:     #long enough to hold
-                    start_l.append(start_loc)
-                    end_l.append(end_loc)
-                else:   #not long enough
-                    start_loc = -1
-                    end_loc = -1
-
-            i+=1
-        if start_loc != -1 and end_loc == -1:
-            end_loc =256
-        if end_loc - start_loc >= p.memory:     #long enough to hold
-            start_l.append(start_loc)
-            end_l.append(end_loc)
-
-        if len(start_l)>0:
-            suitable = 0
-            for i in range(0,len(start_l)):
-                if end_l[i]-start_l[i] < end_l[suitable]-start_l[suitable]:
-                    suitable = i
-            add_memory_exec(start_l[suitable],p)
-            return start_l[suitable]+p.memory
-
-    #not yet return, meaning no enough space
-    return -1  #return -1 for defrag signal
-
-#sub function of add_memory
-def add_memory_exec(start_loc, p):
-    global m_map
-    for i in range(start_loc,start_loc+p.memory):
-        m_map[i] = p.proc_num
-
-
-#call when a process is terminated, clean the memeory
-def remove_memory(p):
-    global m_map
-    for i in range(0,256):
-        if m_map[i] == p.proc_num:
-            m_map[i] = '.'
-
-
-
-#defragmentation function
-#return the cost of defrag
-def defrag():
-    global m_map
-    back_up=[]
-    for i in range(0,256):
-        if m_map[i]!= '.':
-            back_up.append(m_map[i])
-    while len(back_up)!=256:
-        back_up.append('.')
-    count =0
-    for i in range(0,256):
-        if back_up[i]!= '.' and back_up[i]!=m_map[i]:
-            count += 1
-    m_map = back_up
-    return count
